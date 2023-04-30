bc6a849a8ae3ce49fa857ac373aa7a5689eef920
Lucas Bianchezzi Oliveira <lucasbianchezzi700@gmail.com>
2023-04-30 18:39:52 -0300

    Inserido classe Pagamento com propreidades e métodos - atributo Data recebendo Data atual do usuário

---
+++ Pagamento.cs
@@ -0,0 +1,13 @@
+namespace projeto_pagamento
+{
+    public class Pagamento
+    {
+        private DateTime Data { get; set; } = DateTime.Now;
+        public float Valor { get; set; }
+
+        public string Cancelar()
+        {
+            return "Pagamento cancelado com sucesso";
+        }
+    }
+}
