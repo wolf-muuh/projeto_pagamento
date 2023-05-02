cc50d660be935463fbea48f2f54ade79bc84c1a9
Claiton Pereira De Siqueira <29321863885@info.local>
2023-05-02 14:22:31 -0300

    novo progran

--- Program.cs
+++ Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+ 
+float ValorPago = 0;
+int Opcao = 0;
+Console.WriteLine(@$"
+*************************************
+*                                   *
+*    Você está no SEILAOQUEPay      *
+*                                   *
+*************************************");
+do
+{
+    Console.ForegroundColor = ConsoleColor.DarkBlue;
+    Console.WriteLine($"Insira o valor da compra");
+    ValorPago = float.Parse(Console.ReadLine());
+    Console.ResetColor();
+}
+while (ValorPago == 0);
+
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.ResetColor();
+Console.ForegroundColor = ConsoleColor.Red;
+do
+{
+    Console.WriteLine(@$" Escolha a forma de pagamento:
+        |1|---> Boleto
+        |2|---> Credito
+        |3|---> Debito
+        |4|---> Cancelar Operação
+        |0|---> Sair do Sistema");
+    Opcao = int.Parse(Console.ReadLine());
+} while (Opcao != 1 && Opcao != 2 && Opcao != 3 && Opcao != 4 && Opcao != 0);
+Console.ResetColor();
+switch (Opcao)
+{
+    case 1:
+        Console.WriteLine($"parabéns vc escolheu 1");
+        break;
+    case 2:
+        Console.WriteLine($"parabéns vc escolheu 2");
+        
+        break;
+    case 3:
+        Console.WriteLine($"parabéns vc escolheu 3");
+        break;
+    case 4:
+        Console.WriteLine($"parabéns vc escolheu 4");
+        break;
+    case 0:
+        Console.WriteLine($"parabéns vc escolheu 0");
+        break;
+    default:
+        Console.WriteLine($"opcao invalida");
+        break;
+}
+
+Console.WriteLine(@$"
+*************************************
+*                                   *
+*Agradecemos por usar o SEILAOQUEPay*
+*                                   *
+*************************************");
